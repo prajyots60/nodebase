0019f2dcab54c9467ba1ad07a9e7d8e88060dbbc
Prajyot Shinde <prajyotshinde60@gmail.com>
2026-06-04 00:33:32 +0530

    feat: enhance workflow editor with loading/error states, breadcrumbs, and save functionality

--- src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -1,4 +1,15 @@
+import {
+  Editor,
+  EditorError,
+  EditorLoading,
+} from "@/features/editor/components/editor";
+import { EditorHeader } from "@/features/editor/components/editor-header";
+
+import { prefetchWorkflow } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "@sentry/nextjs";
+import { Suspense } from "react";
 
 interface PageProps {
   params: Promise<{
@@ -10,7 +21,20 @@ const Page = async ({ params }: PageProps) => {
   await requireAuth();
   const { workflowId } = await params;
 
-  return <div>Workflow Id : {workflowId}</div>;
+  prefetchWorkflow(workflowId);
+
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<EditorLoading />}>
+        <Suspense fallback={<EditorError />}>
+          <EditorHeader workflowId={workflowId} />
+          <main className="flex-1">
+            <Editor workflowId={workflowId} />
+          </main>
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
 };
 
 export default Page;
