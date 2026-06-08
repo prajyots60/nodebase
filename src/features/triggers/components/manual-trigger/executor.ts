9f174cee80bd5e9d381cd82da2b09eef4253bd07
Prajyot Shinde <prajyotshinde60@gmail.com>
2026-06-08 13:52:44 +0530

    feat: implement HTTP request and manual trigger executors; add executor registry and topological sorting for workflow execution

---
+++ src/features/triggers/components/manual-trigger/executor.ts
@@ -0,0 +1,25 @@
+import type { NodeExecutor } from "@/features/executions/types";
+import { manualTriggerChannel } from "@/inngest/channels/manual-trigger";
+
+type ManualTriggerData = Record<string, unknown>;
+
+export const manualTriggerExecutor: NodeExecutor<ManualTriggerData> = async ({
+  nodeId,
+  context,
+  step,
+  publish,
+}) => {
+  await publish("publish-loading-status", manualTriggerChannel.status, {
+    nodeId,
+    status: "loading",
+  });
+
+  const result = await step.run("manual-trigger", async () => context);
+
+  await publish("publish-success-status", manualTriggerChannel.status, {
+    nodeId,
+    status: "success",
+  });
+
+  return result;
+};
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -3,6 +3,9 @@ import prisma from "@/lib/db";
 import { inngest } from "./client";
 
 import { NonRetriableError } from "inngest";
+import { topologicalSort } from "./utils";
+import { getExecutor } from "@/features/executions/lib/executor-registry";
+import { NodeType } from "@/generated/prisma/enums";
 
 export const executeWorkflow = inngest.createFunction(
   { id: "execute-workflow", triggers: { event: "workflows/execute.workflow" } },
@@ -14,7 +17,7 @@ export const executeWorkflow = inngest.createFunction(
       throw new NonRetriableError("Event ID or workflow ID is missing");
     }
 
-    const nodes = await step.run("prepare-workflow", async () => {
+    const sortedNodes = await step.run("prepare-workflow", async () => {
       const workflow = await prisma.workflow.findUniqueOrThrow({
         where: { id: workflowId },
         include: {
@@ -23,9 +26,39 @@ export const executeWorkflow = inngest.createFunction(
         },
       });
 
-      return workflow.nodes;
+      return topologicalSort(workflow.nodes, workflow.connections);
     });
 
-    return { nodes };
+    const userId = await step.run("find-user-id", async () => {
+      const workflow = await prisma.workflow.findUniqueOrThrow({
+        where: { id: workflowId },
+        select: {
+          userId: true,
+        },
+      });
+
+      return workflow.userId;
+    });
+
+    // Initialize context with any initial data from the trigger
+    let context = event.data.initialData || {};
+
+    // Execute each node
+    for (const node of sortedNodes) {
+      const executor = getExecutor(node.type as NodeType);
+      context = await executor({
+        data: node.data as Record<string, unknown>,
+        nodeId: node.id,
+        userId,
+        context,
+        step,
+        publish: step.realtime.publish,
+      });
+    }
+
+    return {
+      workflowId,
+      context,
+    };
   },
 );
